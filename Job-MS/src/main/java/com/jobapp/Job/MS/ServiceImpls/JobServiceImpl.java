f163a0eea66908c014bf90003543adb5ae9d8660
ritesh <ritesh.jaswal@testingxperts.com>
2024-10-01 14:39:18 +0530

    ADDED RATE LIMITER

--- Job-MS/src/main/java/com/jobapp/Job/MS/ServiceImpls/JobServiceImpl.java
+++ Job-MS/src/main/java/com/jobapp/Job/MS/ServiceImpls/JobServiceImpl.java
@@ -11,6 +11,7 @@ import com.jobapp.Job.MS.Models.External.Company;
 import com.jobapp.Job.MS.Repositories.JobRepo;
 import com.jobapp.Job.MS.Services.JobService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
@@ -43,7 +44,8 @@ public class JobServiceImpl implements JobService
 
     @Override
 //    @CircuitBreaker(name = "companyBreaker",fallbackMethod = "companyBreakerFallback")
-    @Retry(name = "companyBreaker",fallbackMethod = "companyBreakerFallback")
+//    @Retry(name = "companyBreaker",fallbackMethod = "companyBreakerFallback")
+    @RateLimiter(name = "companyBreaker",fallbackMethod = "companyBreakerFallback")
     public List<JobDto> findAll()
     {
         System.out.println("Retry Attempt: "+ ++reviewAttempt);
